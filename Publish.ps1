1230ad400599283c0f216a7cc9b82b0838322129
Lucas Zottis <63120165+LucasZottis@users.noreply.github.com>
2026-02-17 14:15:05 -0300

    Refactor Publish.ps1 by cleaning up code
    Removed commented-out parameters and versioning logic, and updated module import statements.

--- Publish.ps1
+++ Publish.ps1
@@ -2,50 +2,25 @@ param(
     [Parameter(Mandatory = $true)]
     [ValidateSet("major", "minor", "patch")]
     [string]$Bump
-    
-    # [Parameter(Mandatory = $true)]
-    # [string]$Project,
-    
-    # [Parameter(Mandatory = $false)]
-    # [string]$Blazor
 )
 
 $ErrorActionPreference = "Stop"
 
-# Importa módulo pelo caminho (ajuste se necessário)
-Import-Module "$PSScriptRoot\publish-tools.psm1" -Force
+# Importação de módulos
+Import-Module "$PSScriptRoot\git-functions.psm1" -Force
 
-Write-Host "========== PUBLISH =========="
+## Write-Host "========== PUBLISH =========="
 
 # Garante que o repositório está limpo
 Ensure-CleanWorkingTree
 
-Write-Host "Verificando branch atual"
-$releaseBranch = Get-ReleaseBranch
-$currentBranch = Get-CurrentBranch
+# Diretório onde o usuário executou o comando
+$executionRoot = (Get-Location).Path
 
-if ($currentBranch -ne $releaseBranch) {
-    Write-Host "Trocando para '$releaseBranch'..."
-    Switch-ToBranch -Branch $releaseBranch
-}
+# Arquivo de configuração que está no diretório de execução
+$publishSettings = Get-PublishSettings -path $executionRoot
 
-Run-UnitTests
+# Obtém os projetos que serão publicados
+$projects = $publishSettings.Projects
 
-# Obtém versão atual
-$currentVersion = Get-LastVersion
-Write-Host "Versão atual: $currentVersion"
-
-# Calcula nova versão
-$newVersion = Get-BumpedVersion `
-    -CurrentVersion $currentVersion `
-    -Bump $Bump
-
-Write-Host "Nova versão: $newVersion"
-
-Update-VersionInProjects -NewVersion $newVersion
-
-
-
-Commit-VersionUpdate -NewVersion $newVersion
-
-Write-Host "========== RELEASE CONCLUÍDA =========="
+## Write-Host "========== RELEASE CONCLUÍDA =========="
