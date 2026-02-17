6ae69cbc1efcb891e9ffb32b99ef17818a81bef0
Lucas Zottis <63120165+LucasZottis@users.noreply.github.com>
2026-02-17 13:15:41 -0300

    Criado módulo dotnet-functions.psm1
    Guarda as funções relacionadas ao dotnet

---
+++ dotnet-functions.psm1
@@ -0,0 +1,54 @@
+function Update-VersionInProjects {
+    param(
+        [Parameter(Mandatory)]
+        [string]$NewVersion
+    )
+
+    $projects = Get-ChildItem -Recurse -Filter *.csproj |
+                Where-Object { $_.FullName -notmatch "Test" }
+
+    foreach ($proj in $projects) {
+
+        # Write-Host "Atualizando versão em $($proj.Name)..."
+
+        [xml]$xml = Get-Content $proj.FullName
+
+        $propertyGroup = $xml.Project.PropertyGroup |
+                         Where-Object { $_.Version } |
+                         Select-Object -First 1
+
+        if ($propertyGroup) {
+            $propertyGroup.Version = $NewVersion
+        }
+        # else {
+        #     # adiciona dentro do primeiro PropertyGroup
+        #     $pg = $xml.Project.PropertyGroup[0]
+
+        #     if (-not $pg) {
+        #         $pg = $xml.CreateElement("PropertyGroup")
+        #         $xml.Project.AppendChild($pg) | Out-Null
+        #     }
+
+        #     $versionNode = $xml.CreateElement("Version")
+        #     $versionNode.InnerText = $NewVersion
+        #     $pg.AppendChild($versionNode) | Out-Null
+        # }
+
+        $xml.Save($proj.FullName)
+    }
+}
+
+
+# Executa testes unitários
+function Run-UnitTests {
+
+    Write-Host "Executando testes unitários..."
+
+    dotnet test --configuration Release
+
+    if ($LASTEXITCODE -ne 0) {
+        throw "Testes unitários falharam. Release abortado."
+    }
+}
+
+Export-ModuleMember -Function *
