d1027c0f4df7b22697a43582db55c787145221b3
Lucas Zottis <63120165+LucasZottis@users.noreply.github.com>
2026-02-24 14:07:53 -0300

    Update publish-api.ps1

--- scripts/publish-api.ps1
+++ scripts/publish-api.ps1
@@ -7,45 +7,33 @@ param(
 )
 
 try {
+    Write-Title "Projeto API: $($Project.Name)"
+    
     # # Importação de módulos
     Import-Module "$PSScriptRoot\..\modules\DotnetFunctions.psm1" -Force
-
-    Write-Title "Projeto API: $($Project.Name)"
 
     # Executa testes unitários
     Start-UnitTests
 
-    # Obtém última versão
-    # $lastVersion = Get-LastVersion
-
-    # Obtém nova versão
-    # $newVersion = Get-BumpedVersion -CurrentVersion $lastVersion -Bump $Global:Bump
-
-    $directory = Split-Path $Project.Path -Parent
-
     # Atualiza versão nos projetos
+    $directory = Split-Path $Project.Path -Parent
     Update-VersionInProjects -NewVersion $NewVersion -Path $directory
-
-    $scriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
 
     # BEFORE
     if ($Project.Scripts -and $Project.Scripts.Before) {
-
+        Invoke-PublishScripts -Scripts $Project.Scripts.Before
     }
 
     $projectPath = (Resolve-Path $Project.Path).Path
     $outputPath = [System.IO.Path]::GetFullPath($Project.PublishPath)
-
     Start-ApiPublish -ProjectPath $projectPath -OutputPath $outputPath
 
     # AFTER
     if ($Project.Scripts -and $Project.Scripts.After) {
-        foreach ($script in $Project.Scripts.After) {
-            Invoke-Script -ScriptConfig $script -ScriptRoot $scriptRoot
-        }
+        Invoke-PublishScripts -Scripts $Project.Scripts.After
     }
 
-    # Write-Host "✔ $($Project.Name) publicado com sucesso!" -ForegroundColor Green
+    Write-Success "✔ $($Project.Name) publicado com sucesso!"
 }
 catch {
     Write-Error $_
